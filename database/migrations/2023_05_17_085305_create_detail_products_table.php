7bb2ae4f7098216198a05ba0e713bf538bd5701d
mhdtaufikrm <mtaufikramadhan27@gmail.com>
2023-05-17 16:16:40 +0700

    Generating Migrations

---
+++ database/migrations/2023_05_17_085305_create_detail_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('list_products')->onDelete('cascade');
+            $table->text('name');
+            $table->text('description');
+            $table->decimal('price', 8, 2);
+            $table->string('origin'); // Menambahkan kolom 'origin'
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_products');
+    }
+    
+}
